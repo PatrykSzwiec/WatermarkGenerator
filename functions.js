57cb4d029aaeb09497bdc5820e27026625ded804
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-07-31 17:57:09 +0200

    Repair bugfixes, add gitignore

--- functions.js
+++ functions.js
@@ -5,53 +5,53 @@ const inquirer = require('inquirer');
 // FUNCTION WHICH AFFECT ON IMAGE
 const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
   // Check if the source file exists
- const image = await Jimp.read(inputFile);
- const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+  const image = await Jimp.read(inputFile);
+  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
 
- const textData = {
-   text,
-   alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-   alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
- };
- image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
-   await image.quality(100).writeAsync(outputFile);
+  const textData = {
+    text,
+    alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+    alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+  };
+  image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
+  await image.quality(100).writeAsync(outputFile);
 };
 
 const addImageWatermarkToImage = async function(inputFile, outputFile, watermarkFile) {
- const image = await Jimp.read(inputFile);
- const watermark = await Jimp.read(watermarkFile);
- const x = image.getWidth() / 2 - watermark.getWidth() / 2;
- const y = image.getHeight() / 2 - watermark.getHeight() / 2;
+  const image = await Jimp.read(inputFile);
+  const watermark = await Jimp.read(watermarkFile);
+  const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+  const y = image.getHeight() / 2 - watermark.getHeight() / 2;
 
- image.composite(watermark, x, y, {
-   mode: Jimp.BLEND_SOURCE_OVER,
-   opacitySource: 0.5,
- });
- await image.quality(100).writeAsync(outputFile);
+  image.composite(watermark, x, y, {
+    mode: Jimp.BLEND_SOURCE_OVER,
+    opacitySource: 0.5,
+  });
+  await image.quality(100).writeAsync(outputFile);
 };
 
 const addInvertToImage = async function(inputFile, outputFile){
- const image = await Jimp.read(inputFile);
- image.invert();
- await image.quality(100).writeAsync(outputFile);
-}
+  const image = await Jimp.read(inputFile);
+  image.invert();
+  await image.quality(100).writeAsync(outputFile);
+  }
 
 const addGreyScaleToImage = async function(inputFile, outputFile){
- const image = await Jimp.read(inputFile);
- image.greyscale();
- await image.quality(100).writeAsync(outputFile);
+  const image = await Jimp.read(inputFile);
+  image.greyscale();
+  await image.quality(100).writeAsync(outputFile);
 }
 
 const addContrastToImage = async function(inputFile, outputFile, value){
- const image = await Jimp.read(inputFile);
- image.contrast(value);
- await image.quality(100).writeAsync(outputFile);
+  const image = await Jimp.read(inputFile);
+  image.contrast(value);
+  await image.quality(100).writeAsync(outputFile);
 }
 
 const addBrighnessToImage = async function(inputFile, outputFile, value){
- const image = await Jimp.read(inputFile);
- image.brightness(value);
- await image.quality(100).writeAsync(outputFile);
+  const image = await Jimp.read(inputFile);
+  image.brightness(value);
+  await image.quality(100).writeAsync(outputFile);
 }
 
 // VALIDATE FILES
